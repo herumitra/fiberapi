9756a92d14516b634e452a451445bc9f381775fc
Heru Oktafian <heru@Herus-Mac-mini.local>
2024-10-15 19:52:51 +0700

    Update response format of units

--- controllers/unit_controller.go
+++ controllers/unit_controller.go
@@ -19,20 +19,48 @@ func CreateUnit(c *fiber.Ctx) error {
 
 // Function GetUnits
 func GetUnits(c *fiber.Ctx) error {
-	return c.SendStatus(200)
+	// Set format response
+	response := helpers.Response{
+		Status:  "success",
+		Message: "Unit retreived successfully",
+		Data:    "", //&units,
+	}
+	// Return response with status Created
+	return c.Status(fiber.StatusOK).JSON(response)
 }
 
 // Function ShowUnit
 func ShowUnit(c *fiber.Ctx) error {
-	return c.SendStatus(200)
+	// Set format response
+	response := helpers.Response{
+		Status:  "success",
+		Message: "Unit retreived successfully",
+		Data:    "", //&units,
+	}
+	// Return response with status Created
+	return c.Status(fiber.StatusOK).JSON(response)
 }
 
 // Function UpdateUnit
 func UpdateUnit(c *fiber.Ctx) error {
-	return c.SendStatus(200)
+	// Set format response
+	response := helpers.Response{
+		Status:  "success",
+		Message: "Unit updated successfully",
+		Data:    "", //&units,
+	}
+	// Return response with status Created
+	return c.Status(fiber.StatusOK).JSON(response)
 }
 
 // Function DeleteUnit
 func DeleteUnit(c *fiber.Ctx) error {
-	return c.SendStatus(200)
+	// Set format response
+	response := helpers.Response{
+		Status:  "success",
+		Message: "Unit deleted successfully",
+		Data:    "", //&units,
+	}
+	// Return response with status Created
+	return c.Status(fiber.StatusOK).JSON(response)
 }
