9978eb70ecf32fd6adb5100e2927667b376a14f1
HeruOktafian <heru.oktafian@yahoo.com>
2024-10-18 23:51:02 +0700

    update go modules

--- models/product.go
+++ models/product.go
@@ -7,6 +7,6 @@ type Product struct {
 	Description string `gorm:"type:text;not null" json:"description" validate:"required"`
 	PlaceId     string `gorm:"type:varchar(19);not null" json:"place_id" validate:"required"`
 	UnitId      string `gorm:"type:varchar(19);not null" json:"unit_id" validate:"required"`
-	Category    string `gorm:"type:enum('OBAT BATUK','OBAT PILEK','OBAT DEMAM','OBAT PANAS','OBAT PENCERNAAN','OBAT SESAK NAPAS','OBAT MASUK ANGIN','OBAT MATA','OBAT KUMUR','OBAT LUAR','OBAT KULIT','OBAT TETES','SIRUP','DROP','SUPLEMEN','HERBAL','MAKANAN','MINUMAN','SUSU BUBUK','JASA','ALKES','PEMBALUT','POPOK BAYI','POPOK DEWASA','UNDERPAD','REPRODUKSI','LAINNYA');not null" json:"category" validate:"required"`
-	BranchId    string `gorm:"type:varchar(19);not null" json:"branch_id" validate:"required"`
+	// Category    string `gorm:"type:enum('OBAT BATUK','OBAT PILEK','OBAT DEMAM','OBAT PANAS','OBAT PENCERNAAN','OBAT SESAK NAPAS','OBAT MASUK ANGIN','OBAT MATA','OBAT KUMUR','OBAT LUAR','OBAT KULIT','OBAT TETES','SIRUP','DROP','SUPLEMEN','HERBAL','MAKANAN','MINUMAN','SUSU BUBUK','JASA','ALKES','PEMBALUT','POPOK BAYI','POPOK DEWASA','UNDERPAD','REPRODUKSI','LAINNYA');not null" json:"category" validate:"required"`
+	BranchId string `gorm:"type:varchar(19);not null" json:"branch_id" validate:"required"`
 }
